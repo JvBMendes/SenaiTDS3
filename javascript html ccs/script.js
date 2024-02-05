12923e46e0b2ca4e500059cb0f3eb9e53254fd9f
jvbmendes <kdobarcelos@gmail.com>
2024-02-05 17:10:02 -0300

    Blocos de Operações Matemáticas

---
+++ javascript html ccs/script.js
@@ -0,0 +1,35 @@
+function Somar(){
+    var num1 = document.getElementById("num1").valueAsNumber;
+    var num2 = document.getElementById("num2").valueAsNumber;
+    var res = num1+num2;
+    document.getElementById("resSoma").textContent = res.toFixed(3)
+}
+
+function Subtrair(){
+    var num1 = document.getElementById("num3").valueAsNumber;
+    var num2 = document.getElementById("num4").valueAsNumber;
+    var res= num1-num2;
+    document.getElementById("resSub").textContent = res.toFixed(3)
+}
+
+function Dividir(){
+    var num1 = document.getElementById("num5").valueAsNumber;
+    var num2 = document.getElementById("num6").valueAsNumber;
+    var res = num1/num2;
+    document.getElementById("resDiv").textContent = res.toFixed(3)
+}
+
+function Multiplicar(){
+    var num1 = document.getElementById("num7").valueAsNumber;
+    var num2 = document.getElementById("num8").valueAsNumber;
+    var res = num1*num2;
+    document.getElementById("resMult").textContent = res.toFixed(3)
+}
+
+function Media(){
+    var num1 = document.getElementById("num9").valueAsNumber;
+    var num2 = document.getElementById("num10").valueAsNumber;
+    var num3 = document.getElementById("num11").valueAsNumber;
+    var res = (num1+num2+num3)/3;
+    document.getElementById("resMedia").textContent = res.toFixed(3)
+}
